1204f281b51b5859865d196f0bd8589d7316dbed
jguichard-PKL <jerome.guichard@peaklane.fr>
2026-08-04 17:32:34 +0200

    initial commit of custom DS
    Initial commit of custom reports

---
+++ custom_delivery_report/models/stock_picking.py
@@ -0,0 +1,47 @@
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def bl_get_addressee(self):
+        """Invoice address of the linked order (top-right block)."""
+        self.ensure_one()
+        return self.sale_id.partner_invoice_id or self.partner_id
+
+    def bl_get_ship_to(self):
+        """Delivery address of the linked order ("Lieu de livraison" block)."""
+        self.ensure_one()
+        return self.sale_id.partner_shipping_id or self.partner_id
+
+    def bl_get_report_date_str(self):
+        self.ensure_one()
+        date = self.date_done or self.scheduled_date
+        if not date:
+            return ''
+        date = fields.Datetime.context_timestamp(self, date)
+        return date.strftime('%d/%m/%y')
+
+    def bl_get_currency(self):
+        self.ensure_one()
+        return self.sale_id.currency_id or self.company_id.currency_id
+
+    def bl_get_lines(self):
+        """Move lines to display on the report, in a stable order."""
+        self.ensure_one()
+        return self.move_line_ids.filtered(lambda l: l.quantity > 0).sorted('id')
+
+    def bl_get_line_price_unit(self, move_line):
+        sale_line = move_line.move_id.sale_line_id
+        return sale_line.price_unit if sale_line else 0.0
+
+    def bl_get_line_amount(self, move_line):
+        return move_line.quantity * self.bl_get_line_price_unit(move_line)
+
+    def bl_get_total_qty(self):
+        self.ensure_one()
+        return sum(self.bl_get_lines().mapped('quantity'))
+
+    def bl_get_total_amount(self):
+        self.ensure_one()
+        return sum(self.bl_get_line_amount(line) for line in self.bl_get_lines())
